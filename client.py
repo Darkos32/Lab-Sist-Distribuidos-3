cabc487c23e859180bae781f468a1ffddc3a21d5
Jorge Oliveira <jorgeoliv32@gmail.com>
2022-05-02 22:20:28 -0300

    cliente modularizado

--- client.py
+++ client.py
@@ -3,15 +3,24 @@ from socket import *
 HOST = 'localhost'
 PORT = 5000
 
-s = socket(AF_INET, SOCK_STREAM)
-s.connect((HOST, PORT))
-arquivo = input("Diga o nome do arquivo: ")  # recebe o nome do arquivo
-s.send(str.encode(arquivo))  # envia o nome do arquivo para o servidor
-# decodifica a mensagem recebida do servidor. O servidor manda o vetor na forma string, a função eval converte novamente para o vetor
-resposta = eval(s.recv(1024).decode("utf-8"))
+#Inicializa o socket
+def init_socket():
+    socket = socket(AF_INET, SOCK_STREAM)
+    socket.connect((HOST, PORT))
+    return socket
 
-print("As palavras mais recorrentes são:")
-for palavra in resposta:
-    if palavra != None:
-        print(palavra)
-s.close()
+#Imprime a resposta do servidor
+def print_top5(resposta):
+    print("As palavras mais recorrentes são:")
+    for palavra in resposta:
+        if palavra != None:
+            print(palavra)
+
+def main():
+    s = init_socket()
+    arquivo = input("Diga o nome do arquivo: ")  # recebe o nome do arquivo
+    s.send(str.encode(arquivo))  # envia o nome do arquivo para o servidor
+    # decodifica a mensagem recebida do servidor. O servidor manda o vetor na forma string, a função eval converte novamente para o vetor
+    resposta = eval(s.recv(1024).decode("utf-8"))
+    print_top5(resposta)
+    s.close()
